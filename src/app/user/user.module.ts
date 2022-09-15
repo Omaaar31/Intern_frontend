475eac53d1afb257c7055831695911650ad12be1
omar <omarboulahbal@gmail.com>
2022-09-15 10:16:09 +0200

    add Component Service and Model for user

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { SharedModule } from '../shared/shared.module';
+import { UserSigninComponent } from './components/user-signin/user-signin.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [UserSigninComponent],
+  imports: [CommonModule, SharedModule],
 })
-export class UserModule { }
+export class UserModule {}
