8ab8afcef9daf2165a50d5728af564b5dd794908
omar <omarboulahbal@gmail.com>
2022-09-15 13:57:42 +0200

    create form user

--- src/app/user/components/user-signin/user-signin.component.ts
+++ src/app/user/components/user-signin/user-signin.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-signin',
   templateUrl: './user-signin.component.html',
-  styleUrls: ['./user-signin.component.scss']
+  styleUrls: ['./user-signin.component.scss'],
 })
 export class UserSigninComponent implements OnInit {
+  public signinForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
+    this.signinForm = this.formBuilder.group({
+      login: [
+        '', //Default value of login
+        Validators.required,
+      ],
+      pass: ['', Validators.required],
+    });
   }
 
+  public onSubmit(): void {
+    this.userService.signin(this.signinForm.value);
+  }
 }
