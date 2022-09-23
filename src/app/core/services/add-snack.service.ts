11e2a8ab810d0f6e09a3d38d6a2750122c3ab97a
omar <omarboulahbal@gmail.com>
2022-09-23 14:56:11 +0200

    update add-snackbar file

--- src/app/core/services/add-snack.service.ts
+++ src/app/core/services/add-snack.service.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AddSnackService {
-  private readonly config: MatSnackBarConfig = {
-    duration: 3000,
-    //verticalPosition: 'top',
-  };
+  private message: string;
+  private action?: string;
 
-  constructor(private snackBar: MatSnackBar) {}
+  private static duration: number = 3;
 
-  public showSnackBar(message: string): void {
-    this.snackBar.open(message, '', this.config);
+  constructor(private snackBar: MatSnackBar) {
+    this.message = 'message';
+    this.action = 'action';
+  }
+
+  public config(message: string, action?: string) {
+    this.message = message;
+    this.action = action;
+  }
+
+  public open(): void {
+    this.snackBar.open(this.message, this.action, {
+      duration: AddSnackService.duration * 1000,
+    });
   }
 }
