4e13ef9624a2c61997cb1e7721be982f4a528b94
omar <omarboulahbal@gmail.com>
2022-09-23 14:51:48 +0200

    update method onSubmit()

--- src/app/poe/components/poe-add/poe-add.component.ts
+++ src/app/poe/components/poe-add/poe-add.component.ts
@@ -21,7 +21,7 @@ export class POEAddComponent implements OnInit {
 
   ngOnInit(): void {
     this.poeForm = this.formBuilder.group({
-      title: [
+      name: [
         '', //Le boutton submit se grise si aucun champ n'a été rempli
         Validators.compose([Validators.required, Validators.minLength(2)]),
       ],
@@ -35,12 +35,17 @@ export class POEAddComponent implements OnInit {
     //We'll have to create a new Intern Instance
     const poe: POE = new POE();
     poe.id = nextId;
-    poe.title = this.poeForm.value.title;
+    poe.name = this.poeForm.value.name;
 
     //We'll have to pass brand new intern to the add method of our service
     this.poeService.add(poe);
 
-    this.snackBar.showSnackBar('La POE a été ajoutée');
+    this.snackBar.config(
+      `Intern was successfully added`,
+      `Got
+    It`
+    );
+    this.snackBar.open();
 
     //Finally go to the intern table component
     this.router.navigate(['/', 'poes']);
