5d4612c42bafc19b6c414e50923cd1b7b2788acc
omar <omarboulahbal@gmail.com>
2022-09-23 14:53:41 +0200

    Finalize Validators with error in html file and onSubmit()

--- src/app/intern/components/intern-add/intern-add.component.ts
+++ src/app/intern/components/intern-add/intern-add.component.ts
@@ -1,43 +1,96 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { take } from 'rxjs/operators';
+import { Logger } from 'src/app/core/helpers/logger.spec';
 import { Intern } from 'src/app/core/models/intern';
+import { POE } from 'src/app/core/models/poe';
+import { AddSnackService } from 'src/app/core/services/add-snack.service';
 import { InternService } from 'src/app/core/services/intern.service';
+import { POEService } from 'src/app/core/services/poe.service';
+import { DateValidator } from 'src/app/core/validators/date-validator';
 
 @Component({
   selector: 'app-intern-add',
   templateUrl: './intern-add.component.html',
   styleUrls: ['./intern-add.component.scss'],
 })
-export class InternAddComponent implements OnInit {
+export class InternAddComponent implements OnInit, OnDestroy {
   public internForm!: FormGroup;
+  private subscription!: Subscription;
+  public poes!: POE[];
   constructor(
     private formBuilder: FormBuilder,
     private internService: InternService,
-    private router: Router
+    private poeService: POEService,
+    private router: Router,
+    private snackBar: AddSnackService
   ) {}
 
   ngOnInit(): void {
-    this.internForm = this.formBuilder.group({
-      name: [
-        '', //Le boutton submit se grise si aucun champ n'a été rempli
-        Validators.compose([Validators.required, Validators.minLength(2)]),
-      ],
-    });
+    this.poeService
+      .findAll()
+      .pipe(take(1))
+      .subscribe((poes: POE[]) => {
+        Logger.info(`Got ${poes.length} poes`);
+        this.poes = poes;
+        this.internForm = this.formBuilder.group({
+          name: [
+            '', // Default value for the field control
+            [Validators.required, Validators.minLength(2)],
+          ],
+
+          email: new FormControl(
+            '',
+            Validators.compose([
+              Validators.required,
+              Validators.pattern(
+                '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'
+              ),
+            ])
+          ),
+          firstName: [
+            '',
+            Validators.compose([Validators.required, Validators.minLength(2)]),
+          ],
+          phoneNumber: [
+            '',
+            Validators.compose([
+              Validators.required,
+              Validators.pattern(/[0-9\+\-\ ]/),
+            ]),
+          ],
+
+          birthDate: ['', [Validators.required, DateValidator.dateNotLessThan]],
+          poes: ['', Validators.required],
+        });
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
   public onSubmit(): void {
-    console.log(`About to send : ${JSON.stringify(this.internForm.value)}`);
-    const nextId: number = this.internService.getNextId();
+    console.log(`Bout to send : ${JSON.stringify(this.internForm!.value)}`);
 
-    //We'll have to create a new Intern Instance
-    const intern: Intern = new Intern();
-    intern.id = nextId;
-    intern.name = this.internForm.value.name;
+    // We'll have to pass brand new intern to the add method of our service
+    this.subscription = this.internService
+      .add(this.internForm!.value)
+      .subscribe((intern: Intern) => {
+        Logger.info(`An intern was created : ${JSON.stringify(intern)}`);
+        // Load a snack
+        this.snackBar.config(`Intern was successfully added`, `Got It`);
+        this.snackBar.open();
 
-    //We'll have to pass brand new intern to the add method of our service
-    this.internService.add(intern);
-
-    //Finally go to the intern table component
-    this.router.navigate(['/', 'interns']);
+        // Finally go to the intern table component
+        this.router.navigate(['/', 'interns']);
+      });
   }
 }
