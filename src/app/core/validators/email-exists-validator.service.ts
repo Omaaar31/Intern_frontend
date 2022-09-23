c46d4b5607e4402c01ac7f778d877fe77c9ea78c
omar <omarboulahbal@gmail.com>
2022-09-23 15:29:40 +0200

    create emailValidator

---
+++ src/app/core/validators/email-exists-validator.service.ts
@@ -0,0 +1,34 @@
+import { HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { take } from 'rxjs';
+import { Logger } from '../helpers/logger.spec';
+import { InternService } from '../services/intern.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmailExistsValidatorService {
+  constructor(private internService: InternService) {}
+
+  public alreadyExists(
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> {
+    const validationError: ValidationErrors = { alreadyExists: true };
+    return new Promise((emailExists) => {
+      this.internService
+        .emailAlreadyExists(control.value)
+        .pipe(take(1))
+        .subscribe({
+          next: (response: HttpResponse<any>) => {
+            Logger.info(`Got a ${response.status} so, all is okay`);
+            emailExists(null);
+          },
+          error: (error: any) => {
+            Logger.error(`email ${control.value} is already taken`);
+            emailExists(validationError);
+          },
+        });
+    });
+  }
+}
