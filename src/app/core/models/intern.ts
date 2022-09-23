bc860a1c6753d1ad15177c252fe74d842f01c280
omar <omarboulahbal@gmail.com>
2022-09-23 14:56:26 +0200

    deserialize intern

--- src/app/core/models/intern.ts
+++ src/app/core/models/intern.ts
@@ -1,13 +1,35 @@
+import { Expose, plainToInstance, Type } from 'class-transformer';
+import 'reflect-metadata';
 export class Intern {
+  @Expose()
   public id?: number;
+
+  @Expose()
   public name: string;
+
+  @Expose()
   public firstName?: string;
+
+  @Expose()
   public phoneNumber?: string;
+
+  @Expose()
   public email?: string;
+
+  @Expose()
+  @Type()
   public birthDate?: Date;
+
+  @Expose()
   public adress?: string;
 
   public constructor() {
     this.name = '';
+  }
+
+  public deserialize(plainIntern: any): Intern {
+    return plainToInstance(Intern, plainIntern, {
+      excludeExtraneousValues: true,
+    });
   }
 }
