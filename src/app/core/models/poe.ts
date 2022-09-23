ab5a19d38b66c0c976fe3ec8b5ad1f46c7309ed0
omar <omarboulahbal@gmail.com>
2022-09-23 14:56:33 +0200

    deserialize poe

--- src/app/core/models/poe.ts
+++ src/app/core/models/poe.ts
@@ -1,6 +1,24 @@
+import { Expose, plainToInstance, Type } from 'class-transformer';
+import 'reflect-metadata';
 export class POE {
+  @Expose()
   public id?: number;
-  public title!: string;
+
+  @Expose()
+  public name!: string;
+
+  @Expose()
+  @Type()
   public beginDate?: Date;
+
+  @Expose()
+  @Type()
   public endDate?: Date;
+
+  public deserialize(plainPOE: any): POE {
+    const asClass: POE = plainToInstance(POE, plainPOE, {
+      excludeExtraneousValues: true,
+    });
+    return asClass;
+  }
 }
