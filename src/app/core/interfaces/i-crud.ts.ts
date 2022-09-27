1542fb72abeba111ec42351223042c6508384939
omar <omarboulahbal@gmail.com>
2022-09-27 11:40:53 +0200

    update ICrud

--- src/app/core/interfaces/i-crud.ts.ts
+++ src/app/core/interfaces/i-crud.ts.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs';
 
 export interface ICrud<T> {
   add(item: T): void;
-  update(item: T): void;
+  update(id: number, item: T): void;
   delete(item: T): void;
   findAll(): Observable<T[]>;
   findOne(id: number): Observable<T | null>;
