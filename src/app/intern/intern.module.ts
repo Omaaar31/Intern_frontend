a2225ca156732c3b8d618f98db5b4bbd15799d2c
omar <omarboulahbal@gmail.com>
2022-09-28 14:05:00 +0200

    add internUpdate to intern.module

--- src/app/intern/intern.module.ts
+++ src/app/intern/intern.module.ts
@@ -6,12 +6,14 @@ import { InternDetailComponent } from './components/intern-detail/intern-detail.
 import { InternTableComponent } from './components/intern-table/intern-table.component';
 import { InternAddComponent } from './components/intern-add/intern-add.component';
 import { SharedModule } from '../shared/shared.module';
+import { InternUpdateComponent } from './components/inter-update/intern-update.component';
 
 @NgModule({
   declarations: [
     InternDetailComponent,
     InternTableComponent,
     InternAddComponent,
+    InternUpdateComponent,
   ],
   imports: [CommonModule, InternRoutingModule, SharedModule],
 })
